b250a1625273d64af41fe4f48c71b9ea8f95584e
brookluers <BrookLuers@gmail.com>
2017-08-24 14:43:25 -0400

    PC projections before DOC

---
+++ hmap_knn_pca.R
@@ -0,0 +1,29 @@
+library(ggplot2)
+maxID = 2
+
+
+cat("\n")
+cat("-------R: Plotting heatmaps-------\n")
+lowc = "#fff7bc"
+highc = "#d95f0e"
+fscale = scale_fill_gradient(low = lowc, high=highc,name="Pr(brake | B^t x)")
+
+p_cd <- ggplot()  + geom_tile(aes(x=cd1,y=cd2, fill=ypred)) +
+  xlab("cov dir 1") + ylab("cov dir2") #+
+  #fscale
+p_m <- ggplot() + geom_tile(aes(x=meandir,y=cd1,fill=ypred)) +
+    xlab("mean direction") + ylab("cov dir 1") #+
+    #fscale
+
+for (curID in 1:maxID){
+    h <- read.csv(sprintf("hmap_pca_cd1_cd2_%03d.txt", curID), header=T)
+    ggsave(sprintf("hmap_cd1_cd2_pca_%03d.pdf", curID), 
+    		p_cd %+% h +
+		ggtitle(paste("Driver", curID)),
+		width = 6, height=5.5, units='in')
+    h <- read.csv(sprintf("hmap_pca_meandir_cd1_%03d.txt", curID), header=T)
+    ggsave(sprintf("hmap_meandir_cd1_pca_%03d.pdf", curID),
+    	    p_m %+% h +
+	    ggtitle(paste("Driver", curID)),
+	    width = 6, height=5.5, units='in')
+}
