aad0477ba7630552ff10b77ea84b34bf89e0ab22
brookluers <BrookLuers@gmail.com>
2017-09-09 10:35:46 -0400

    count number of brakes/observations per driver

---
+++ ycounts.py
@@ -0,0 +1,14 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import dask.dataframe as dd
+import numpy as np
+import pandas as pd
+
+df = dd.read_csv('smproj_multi_*.txt', 
+                 dtype={'Driver': str}, 
+                 usecols = ['Driver', 'Brake'],
+                 assume_missing=True)
+ycounts = df.groupby('Driver')['Brake'].aggregate(['sum','count'])
+ycounts = ycounts.compute()
+ycounts.to_csv('ycounts.txt')
