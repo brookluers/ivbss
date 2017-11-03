9dfab37b8694b0e5a36f6a9d51508755c0e7b3f4
Brook Luers <luers@flux-login3.arc-ts.umich.edu>
2017-11-03 15:33:14 -0400

    histograms by braking

--- proj_histograms.py
+++ proj_histograms.py
@@ -3,24 +3,25 @@ import dask.array as da
 import numpy as np
 import pandas as pd
 
-dirnames = ['meandir0','cd0','cd1']
+dirnames = ['meandir','cd1','cd2']
 cols = ['Driver']
 cols.extend(dirnames)
 
-df = dd.read_csv('psmall_*.txt', 
+df = dd.read_csv('/scratch/stats_flux/luers/scores_8pc_py_*.txt', 
                  dtype={'Driver': str},
                  usecols = cols,
                  assume_missing=True)
 
 minmax = df.groupby('Driver').agg(['min','max']).compute()
 
-maxID = 3
+maxID = 108
 
 b_lwr = [] # bun upper boundaries
 b_upr = [] # bin lower boundaries
 h = [] # histogram counts
 driver = [] #  driver ids
 vname = [] #  variable names
+brake_ind = []
 nbins = 100
 for i in range(1, maxID + 1):
     driver_padded = "{0:03d}".format(i)
@@ -31,16 +32,26 @@ for i in range(1, maxID + 1):
         ch, cb = da.histogram(df[df.Driver==driver_nopad][cur_dir].values,
                               bins = nbins, range = [rg['min'], rg['max']],
                               density=True)
+        ch1, cb1 = da.histogram(df[ ( df.Driver==driver_nopad) & (df.Brake > 0)][cur_dir].values,
+                                bins = nbins, range = [rg['min'], rg['max']],
+                                density=True)
+        ch0, cb0 = da.histogram(df[ ( df.Driver==driver_nopad) & (df.Brake < 0.5 )][cur_dir].values,
+                                bins = nbins, range = [rg['min'], rg['max']],
+                                density=True)
+        ch1 = ch1.compute()
+        ch0 = ch0.compute()
         ch = ch.compute()
-        h.extend(ch)
-        b_lwr.extend(cb[0:(len(cb) - 1)])
-        b_upr.extend(cb[1:len(cb)])
-        vname.extend([cur_dir] * len(ch))
-        driver.extend([i] * len(ch))
+        h.extend(ch).extend(ch1).extend(ch0)
+        b_lwr.extend(cb[0:(len(cb) - 1)]).extend(cb1[0:(len(cb1) - 1)]).extend(cb0[0:(len(cb0) - 1)])
+        b_upr.extend(cb[1:len(cb)]).extend(cb1[1:len(cb1)]).extend(cb0[1:len(cb0)])
+        vname.extend([cur_dir] * (len(ch) + len(ch1) + len(ch0)))
+        driver.extend([i] * (len(ch) + len(ch1) + len(ch0)))
+        brake_ind.extend([-99] * len(ch)).extend([1.0] * len(ch1)).extend([0.0] * len(ch0))
+        
 
-d = {"Driver": driver, "Dir": vname, "bin_lwr": b_lwr, "bin_upr": b_upr, "hdens": h}
+d = {"Driver": driver, "Dir": vname, "bin_lwr": b_lwr, "bin_upr": b_upr, "hdens": h, "Brake": brake_ind}
 res = pd.DataFrame(d)
-res.to_csv("proj_histograms.txt", index=False)
+res.to_csv("/scratch/stats_flux/luers/proj_histograms.txt", index=False)
 
     
     
