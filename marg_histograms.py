09aff99e69aa1ed70c94103ca4106a13672b03eb
brookluers <BrookLuers@gmail.com>
2018-03-12 06:15:35 -0400

    update histograms for new response variable

--- marg_histograms.py
+++ marg_histograms.py
@@ -4,23 +4,20 @@ import numpy as np
 import pandas as pd
 #import numexpr
 #numexpr.set_nthreads(1)
-df = dd.read_csv("smproj_8pc_*.txt",
-                 usecols = ['Speed[0]','FcwRange[0]', 'Brake'],
+df = dd.read_csv("data/lagdat_small*.txt",
+                 usecols = ['Speed[0]','FcwRange[0]', 'Brake_1sec'],
                  assume_missing=True)
-
-sp_h0, sp_bins0  = da.histogram(df[df.Brake==0]['Speed[0]'].values,
+sp_h0, sp_bins0  = da.histogram(df[df.Brake_1sec==0]['Speed[0]'].values,
                       bins=50, range=[7, 40])
-sp_h1, sp_bins1  = da.histogram(df[df.Brake==1]['Speed[0]'].values,
+sp_h1, sp_bins1  = da.histogram(df[df.Brake_1sec==1]['Speed[0]'].values,
                       bins=50, range=[7, 40])
-r_h0, r_bins0 = da.histogram(df[df.Brake==0]['FcwRange[0]'].values,
+r_h0, r_bins0 = da.histogram(df[df.Brake_1sec==0]['FcwRange[0]'].values,
                              bins=50, range=[0, 125])
-r_h1, r_bins1 = da.histogram(df[df.Brake==1]['FcwRange[0]'].values,
+r_h1, r_bins1 = da.histogram(df[df.Brake_1sec==1]['FcwRange[0]'].values,
                              bins=50, range=[0, 125])
 print("Speed[0] histogram, Brake==0")
 print(sp_bins0)
 print(sp_h0.compute())
-
-
 print("\n-------\nSpeed[0] histogram, Brake==1")
 print(sp_bins1)
 print(sp_h1.compute())
@@ -30,7 +27,5 @@ print(r_h0.compute())
 print("\n-------\nRange[0] histogram, Brake==1")
 print(r_bins1)
 print(r_h1.compute())
-
-
 
 
