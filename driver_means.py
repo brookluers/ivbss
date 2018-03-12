9f1963669409a84fc884a9b5152ed738fd7f16e6
brookluers <BrookLuers@gmail.com>
2018-03-12 15:50:32 -0400

    means by driver and y level

---
+++ driver_means.py
@@ -0,0 +1,19 @@
+import dask.dataframe as dd
+import numpy as np
+import pandas as pd
+
+dirfile = pd.read_csv('data/directions_0pc.txt',
+    #'/scratch/stats_flux/luers/directions_0pc.txt',
+                      usecols=['varname'])
+xnames = dirfile.varname.values
+colnames = list(xnames)
+colnames += ["Brake_1sec", "Driver"]
+
+df = dd.read_csv("data/lagdat_small_*",
+    #"/scratch/stats_flux/luers/smproj_8pc_*.txt",
+                 usecols =  colnames,
+                 assume_missing=True)
+
+dfa = df.groupby(['Driver', 'Brake_1sec']).agg([np.mean])
+dfa = dfa.compute().stack().reset_index()
+dfa.to_csv("driver_means.txt")
